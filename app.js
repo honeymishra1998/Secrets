ad3ff5e8c906d2387c27df037e73bae30237ee31
defaultuser0 <honeymishra.1998@gmail.com>
2021-01-09 14:26:38 +0530

    Added Procfile and changed ports

--- app.js
+++ app.js
@@ -197,6 +197,11 @@ app.post("/login", function(req, res){
   });
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if(port == null || port == '') {
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Server is running on port 3000");
 })
